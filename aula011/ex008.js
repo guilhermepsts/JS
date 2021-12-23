9b95c89bedf29ce7306328ae6398a64e6caab345
Guilherme Santos <guilhermegps08@gmail.com>
2021-12-23 17:03:57 -0300

    l

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`Velocidade máxima ultrapassada. MULTADO`)
+}
+console.log(`Dirija sempre usando o cinto de segurança`)
